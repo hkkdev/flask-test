88854e9679a8f0e3db40b9cc374bcb59cdf6a9d6
HKK <hkkdev@outlook.com>
2014-08-31 18:01:11 -0500

    lastest

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,12 @@
 # __init__.py
 
-import os
 from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
-
 
 app = Flask(__name__)
 
-
-basedir = os.path.abspath(os.path.dirname(__file__))
 app.config.update(dict(
     DEBUG=True,
     SECRET_KEY='something',
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 ))
 
-db = SQLAlchemy(app)
-
-from app import views, models
+from app import views
